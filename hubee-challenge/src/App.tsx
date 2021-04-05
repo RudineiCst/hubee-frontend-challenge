bb05719438398a9a59703fc8a9f041b99ee520a6
Rudinei Costa <costaaugustodev@gmail.com>
2021-04-05 17:47:12 -0300

    feat: consumindo api com json server

--- hubee-challenge/src/App.tsx
+++ hubee-challenge/src/App.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import GlobalStyles from './styles/GlobalStyles';
 import Product from './Components/product';
 
+import api from  './server/api';
+
+interface ProductInterface {
+  id: number;
+  name:string;
+  price: number;
+  url: string;
+}
+
 const App: React.FC = ()=>  {
+  const [productItem, setProductItem] = useState<ProductInterface[]>([]);
+
+  async function loadProducts (){
+    const response = await api.get('products');
+
+    const responseProducts = response.data;
+
+    setProductItem([...productItem, responseProducts]);
+    console.log(responseProducts);
+  }
+  useEffect(()=> {
+   loadProducts();
+  },[]);
+
   return (
     <>
+
       <Product/>
       <Product/>
       
